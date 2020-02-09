8c4fae45d6df42635fcecabebf0a7c510bd24a92
aakond <aak168@tpu.ru>
2020-02-10 02:00:12 +0700

    NoteForm component added

--- src/components/NotesPage.tsx
+++ src/components/NotesPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import NoteForm from './NoteForm'
 
 import '../styles/styles.scss'
 
 const NotesPage: React.FC = () => {
-        return <h4 className="align-center">Ваши заметки</h4>
+    return <div className="container"><h4 className="align-center">
+        Ваши заметки
+        </h4>
+        <NoteForm/></div>
+
 }
 
 export default NotesPage;
