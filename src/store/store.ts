e2294452c864a74f3be7181696a778e4e53831a5
aakond <aak168@tpu.ru>
2020-02-12 01:31:32 +0700

    Store creation changed

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import rootReducer from "../reducers";
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools(
-    applyMiddleware(),
-  ));
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
