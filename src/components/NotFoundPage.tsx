7f2fe31b3952fdd4ad4da90defedfcdd130789ec
aakond <aak168@tpu.ru>
2020-02-14 02:37:45 +0700

    Tags fixed on NoteFoundPage

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -4,8 +4,8 @@ import '../styles/styles.scss'
 
 const NotFoundPage: React.FC = () => {
     return <div className="align-center">
-        <h1>Ой! &#x1F47B;</h1>
-        <h3>Запрошенная страница не существует</h3>
+        <h2>Ой! &#x1F47B;</h2>
+        <h4>Запрошенная страница не существует</h4>
     </div>
 
 }
