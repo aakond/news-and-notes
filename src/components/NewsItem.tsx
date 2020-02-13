4d48d0b6ec337f8fa4d6ab0e1d44cd55b97a078c
aakond <aak168@tpu.ru>
2020-02-14 04:36:18 +0700

    Content on the NewsPage added

---
+++ src/components/NewsItem.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import '../styles/styles.scss'
+
+const NewsItem: React.FC = () => {
+    return <div className="card">
+
+        <div className="card-image">
+          <img src="src\images\sample-1.jpg"/>
+          <span className="card-title">01/10/2020</span>
+        </div>
+        <div className="card-content ">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div className="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+};
+
+export default NewsItem;
--- src/components/NewsPage.tsx
+++ src/components/NewsPage.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import NewsItem from './NewsItem';
 
 const NewsPage: React.FC = () => {
-        return <h1>There will be News</h1>
+    return <div >
+        <h4 className="align-center">
+            Новости
+        </h4>
+        <div className="row">
+            <div className="col s12 m4 l2"><NewsItem /></div>
+            <div className="col s12 m4 l8"><NewsItem /></div>
+            <div className="col s12 m4 l2"><NewsItem /></div>
+            <div className="col s12"><NewsItem /></div>
+        </div>
+        <div className="row">
+            <div className="col s12 m6 l3"><NewsItem /></div>
+            <div className="col s12 m6 l3"><NewsItem /></div>
+            <div className="col s12 m6 l3"><NewsItem /></div>
+            <div className="col s12 m6 l3"><NewsItem /></div>
+        </div>
+    </div>
 }
 
 export default NewsPage;
