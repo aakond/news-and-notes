9171dcf37bf840dc2261dd6aa004eec40bcfdd5c
aakond <aak168@tpu.ru>
2020-02-16 19:55:15 +0700

    Content to MainPage Added

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const MainPage: React.FC = () => {
-        return <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, fugiat ex? Ipsum sequi architecto harum, magni repellendus expedita eius nostrum! Maiores cupiditate error aspernatur officia omnis molestiae, atque dolorem nisi autem quisquam voluptas. Aliquid culpa dolorem tempore dolor, deserunt facere! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, suscipit. Quos ad nobis laudantium eum aliquam aut veritatis alias sed!</h2>
-
+        return <div className="container">
+        <h4 className="align-center">
+            Lorem, ipsum
+        </h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia dignissimos fugit esse laudantium? Pariatur dolorum sequi rerum mollitia facilis nulla suscipit eaque? Id accusantium, incidunt inventore magnam repellat sapiente enim, quam in ipsum quia delectus, deleniti nemo a autem. Corrupti, obcaecati. Aut ad officia quidem vitae optio libero ea minima quasi repellat est! Eius ab iure omnis laboriosam eum eos consectetur dicta impedit perspiciatis, inventore praesentium, iste necessitatibus debitis sit voluptas, repellat explicabo ipsa dolores? Earum eligendi vero iusto, natus ea voluptas quidem aliquam, dolore praesentium autem nesciunt accusantium quasi saepe provident harum modi, porro totam officia veniam. Nisi, voluptatum?</p>
+        <Link to="/somewhere" className="waves-effect waves-light btn-large">Нажмите сюда</Link>
+        </div>
 }
 
 export default MainPage;
